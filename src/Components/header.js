d14bf26026238479b078780f8be313e010de0678
pysaquib <iamsaquibnasim@gmail.com>
2020-11-16 02:06:07 +0530

    some global resetting and converting pixels to rem

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,18 +1,23 @@
-import "./header.css";
+import styles from "./header.module.css";
 import Logo from "../assets/img/logo-white.png";
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="logo-box">
-        <img src={Logo} alt="Logo" className="logo"></img>
+    <header className={styles.header}>
+      <div className={styles.logo_box}>
+        <img src={Logo} alt="Logo" className={styles.logo}></img>
       </div>
-      <div className="text-box">
-        <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-sub">is where life happens</span>
+      <div className={styles.text_box}>
+        <h1 className={styles.heading_primary}>
+          <span className={styles.heading_primary_main}>Outdoors</span>
+          <span className={styles.heading_primary_sub}>
+            is where life happens
+          </span>
         </h1>
-        <a href="#" className="btn btn-white btn-animated">
+        <a
+          href="#"
+          className={`${styles.btn} ${styles.btn_white} ${styles.btn_animated}`}
+        >
           Discover all tours
         </a>
       </div>
